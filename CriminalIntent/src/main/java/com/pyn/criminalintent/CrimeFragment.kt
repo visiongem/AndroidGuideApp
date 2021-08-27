950feba4017861e18720c05eb767fcac89419568
visiongem <393507488@qq.com>
2021-08-27 17:06:14 +0800

    add fragment

--- CriminalIntent/src/main/java/com/pyn/criminalintent/CrimeFragment.kt
+++ CriminalIntent/src/main/java/com/pyn/criminalintent/CrimeFragment.kt
@@ -8,10 +8,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pyn.criminalintent.databinding.CrimeFragmentBinding
 
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
+
+    // fragment 的生命周期与 activity 的生命周期不同，并且该fragment 可以超出其视图的生命周期，因此如果不将其设置为null，则可能会发生内存泄漏。
+    // 另一个变量通过 !! 使一个变量为可空值而使另一个变量为非空值避免了空检查。
+    private var _binding: CrimeFragmentBinding? = null
+    private val mBinding get() = _binding!!
 
     companion object {
         fun newInstance() = CrimeFragment()
@@ -28,7 +34,12 @@ class CrimeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.crime_fragment, container, false)
+        _binding = CrimeFragmentBinding.inflate(layoutInflater, container, false)
+        mBinding.btnCrimeDate.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+        return mBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -41,18 +52,26 @@ class CrimeFragment : Fragment() {
         super.onStart()
         val titleWatcher = object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
+                crime.title = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
 
+            }
         }
+        mBinding.edtCrimeTitle.addTextChangedListener(titleWatcher)
+        mBinding.cboxCrimeSolved.apply {
+            setOnCheckedChangeListener { _, isChecked -> crime.isSolved = isChecked }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- CriminalIntent/src/main/java/com/pyn/criminalintent/MainActivity.kt
+++ CriminalIntent/src/main/java/com/pyn/criminalintent/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.flayout_fragment_container)
+        if(currentFragment == null){
+            val crimeFragment = CrimeFragment.newInstance()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.flayout_fragment_container, crimeFragment)
+                .commit()
+        }
     }
 }
