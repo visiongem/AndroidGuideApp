655b50eec009a99f40890b21aa4576ec1f559ded
visiongem <393507488@qq.com>
2021-08-31 15:04:05 +0800

    add exportSchema = false

--- CriminalIntent/src/main/java/com/pyn/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/src/main/java/com/pyn/criminalintent/database/CrimeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.pyn.criminalintent.bean.Crime
 
-@Database(entities = [Crime::class], version = 1)
+@Database(entities = [Crime::class], version = 1, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao():CrimeDao
