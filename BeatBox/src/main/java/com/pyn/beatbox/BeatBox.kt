9ca83882fed4296a529f995b51e7843e6bc1c1ec
Yane <visiongem@users.noreply.github.com>
2021-11-22 12:29:25 +0800

    update

--- BeatBox/src/main/java/com/pyn/beatbox/BeatBox.kt
+++ BeatBox/src/main/java/com/pyn/beatbox/BeatBox.kt
@@ -1,15 +1,20 @@
 package com.pyn.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder().setMaxStreams(MAX_SOUNDS).build()
 
     init {
         sounds = loadSounds()
@@ -30,8 +35,26 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { fileName ->
             val assetPath = "$SOUNDS_FOLDER/$fileName"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException) {
+                Log.e(TAG, "Counld not load sound $fileName", ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
+    }
+
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            // 音频ID、左音量、右音量、优先级（无效）、是否循环和播放速率
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
     }
 }
--- BeatBox/src/main/java/com/pyn/beatbox/Sound.kt
+++ BeatBox/src/main/java/com/pyn/beatbox/Sound.kt
@@ -2,6 +2,6 @@ package com.pyn.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(private val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
