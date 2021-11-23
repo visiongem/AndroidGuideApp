01df35758e5302fbd7c1971bb5f6fdbf47c1f8cb
Yane <visiongem@users.noreply.github.com>
2021-11-23 12:45:09 +0800

    ok
    ok

---
+++ BeatBox/src/androidTest/java/com/pyn/beatbox/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package com.pyn.beatbox
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun showsFirstFileName(){
+        onView(withText("65_cjipie"))
+            .check(matches(isDisplayed()))
+    }
+}
