4b5a43e94fdf020a315344c63535a8acfd4ee0ee
Yane <393507488@qq.com>
2021-11-23 16:37:36 +0800

    create BeatBoxViewModel.kt

---
+++ BeatBox/src/main/java/com/pyn/beatbox/BeatBoxViewModel.kt
@@ -0,0 +1,12 @@
+package com.pyn.beatbox
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Description：
+ * @author Created by pengyanni
+ * @e-mail 393507488@qq.com
+ * @time   2021/11/23 3:45 下午
+ */
+class BeatBoxViewModel :ViewModel(){
+}
