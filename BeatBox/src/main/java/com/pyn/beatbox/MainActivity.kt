48975b8350e7fb179cb85b4bf0643fe2017b7f77
Yane <visiongem@users.noreply.github.com>
2021-11-25 23:58:40 +0800

    update
    update

--- BeatBox/src/main/java/com/pyn/beatbox/MainActivity.kt
+++ BeatBox/src/main/java/com/pyn/beatbox/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.pyn.beatbox
 
+import android.content.res.Resources
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
@@ -27,13 +29,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private inner class SoundHolder(private val binding:ListItemSoundBinding):RecyclerView.ViewHolder(binding.root){
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         init {
             binding.viewModel = SoundViewModel(beatBox)
         }
 
-        fun bind(sound:Sound){
+        fun bind(sound: Sound) {
             binding.apply {
                 viewModel?.sound = sound
                 executePendingBindings()
@@ -41,29 +44,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private inner class SoundAdapter(private val sounds:List<Sound>):RecyclerView.Adapter<SoundHolder>(){
+    private inner class SoundAdapter(private val sounds: List<Sound>) :
+        RecyclerView.Adapter<SoundHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            SoundHolder{
-                val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
-                    layoutInflater,
-                    R.layout.list_item_sound,
-                    parent,
-                    false
-                )
-                binding.lifecycleOwner = this@MainActivity
-                return SoundHolder(binding)
+                SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            binding.lifecycleOwner = this@MainActivity
+            return SoundHolder(binding)
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
             val sound = sounds[position]
             holder.bind(sound)
         }
+
         override fun getItemCount() = sounds.size
     }
 
     override fun onDestroy() {
         super.onDestroy()
         beatBox.release()
+    }
+
+    fun setThemeColor() {
+        val theme: Resources.Theme = this.theme
+        val attrsToFetch = intArrayOf(R.attr.colorAccent)
+        val a: TypedArray =
+            theme.obtainStyledAttributes(R.style.Theme_AndroidGuideApp, attrsToFetch)
+        val accentColor = a.getInt(0, 0)
+        a.recycle()
     }
 }
