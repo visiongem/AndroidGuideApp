bdf730e35fcef4ff4c29eea9a3fc50f3e709af8c
Nia.Peng <393507488@qq.com>
2023-06-29 17:25:25 +0800

    update 30.4

--- draganddraw/src/main/java/com/yn/draganddraw/BoxDrawingView.kt
+++ draganddraw/src/main/java/com/yn/draganddraw/BoxDrawingView.kt
@@ -21,6 +21,12 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
 
     private var currentBox: Box? = null
     private val boxen = mutableListOf<Box>()
+    private val boxPaint = Paint().apply {
+        color = 0x22ff0000
+    }
+    private val backgroundPaint = Paint().apply {
+        color = 0xfff8efe0.toInt()
+    }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
@@ -52,7 +58,10 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
     }
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+        canvas.drawPaint(backgroundPaint)
+        boxen.forEach { box ->
+            canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
+        }
     }
 
     private fun updateCurrentBox(current: PointF) {
